afe85e536748e98641d5715ab3830be401b4a48c
NeoLePorte <kagatookuda@gmail.com>
2017-05-16 19:58:31 -0400

    added codepen embeds

--- src/Projects.vue
+++ src/Projects.vue
@@ -4,17 +4,21 @@
         <div class="projects">
         <section class="projects-section">
         <p>PROJECTS</p>
-        
         </section>
-        
+        <div class="pens">
+        <p data-height="265" data-theme-id="0" data-slug-hash="rmMoWj" data-default-tab="css,result" data-user="bvsscrvft" data-embed-version="2" data-pen-title="ToDoList" data-preview="true" class="codepen">See the Pen <a href="http://codepen.io/bvsscrvft/pen/rmMoWj/">ToDoList</a> by Anthony Blanton (<a href="http://codepen.io/bvsscrvft">@bvsscrvft</a>) on <a href="http://codepen.io">CodePen</a>.</p>
+
+        <p data-height="265" data-theme-id="0" data-slug-hash="EWXBEg" data-default-tab="js,result" data-user="bvsscrvft" data-embed-version="2" data-pen-title="Glitchwar Sadat" data-preview="true" class="codepen">See the Pen <a href="http://codepen.io/bvsscrvft/pen/EWXBEg/">Glitchwar Sadat</a> by Anthony Blanton (<a href="http://codepen.io/bvsscrvft">@bvsscrvft</a>) on <a href="http://codepen.io">CodePen</a>.</p>
+
+        <p data-height="265" data-theme-id="0" data-slug-hash="yVMopr" data-default-tab="css,result" data-user="bvsscrvft" data-embed-version="2" data-pen-title="Cut-Up Machine" data-preview="true" class="codepen">See the Pen <a href="http://codepen.io/bvsscrvft/pen/yVMopr/">Cut-Up Machine</a> by Anthony Blanton (<a href="http://codepen.io/bvsscrvft">@bvsscrvft</a>) on <a href="http://codepen.io">CodePen</a>.</p>
         </div>
-        
-        
-    </div>
+    </div>   
+</div>
         </transition>
     </template>
 
     <script>
+     
 export default {
       template: '#projects-component-template'
     }    
