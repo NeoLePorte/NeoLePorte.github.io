9537aa48b48e3b91d4f88b94e001c9f08fe4797c
NeoLePorte <kagatookuda@gmail.com>
2017-06-20 23:43:18 -0400

    Fixed page scrolling issue between routes.
    Also added to about page

--- src/About.vue
+++ src/About.vue
@@ -1,12 +1,23 @@
 <template id="about-component-template">
-        <transition name="custom-classes-transition" enter-active-class="animated fadeInLeft" leave-active-class="animated fadeOut" >
+        <transition name="custom-classes-transition" enter-active-class="animated fadeInLeft" >
     <div>
         <div class="about">
-        <section class="about-section">
-        <p>ABOUT ME</p>
-        </section>
-         <p>My name is Anthony Blanton. 
-        </p>
+            <section class="about-section">
+                <div class="pic-container">
+                <transition  name="custom-classes-transition" enter-active-class="animated rollIn">
+                    <img v-if="show" src="css/img/me2.jpg" alt="Me thinking deeply...">
+                </transition>
+                </div>
+
+                <p>Hello, I am a Web Developer out of Louisville, Kentucky.
+                    I work primarly with javascript (front-end/Back-end), and
+                    various frameworks to make cool and useful things. <br>
+
+                <br> Check out some of my <a href="#/Projects">Projects</a> 
+                and <a href="#/Contact">Contact me</a>
+                for any work opportunities or if you have any cool projects I could assist with.
+                </p>
+            </section>
         </div>
     </div>
         </transition>
@@ -15,12 +26,41 @@
 
 <script>
 export default {
-        template: "#about-component-template"
+        template: "#about-component-template",
+        data()  {
+            return {
+                show: true
+            }
+        }
         }    
 </script>
 
 <style>
-    .about {
-    background: rgba(26,27,37,0.7)
+.about {
+background: rgba(26,27,37,0.7)
 }
+
+.about-section > p {
+    border-bottom: 2px solid ivory;
+}
+.about-section {
+    display: flex;
+    flex-direction: column;
+    align-content: flex-end;
+    padding-top: 5%;
+}
+
+.pic-container {
+    display: flex;
+    flex-direction: column;
+    border-bottom: 2px solid ivory;
+}
+
+.pic-container img {
+    max-width: 35%;
+    max-height: 35%;
+    border-radius: 100%;
+    align-self: center;
+}
+
 </style>
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
 <div id="app">
     <header class="main-heading"  v-sticky="{ zIndex: 100, stickyTop: 1 }">
-        <h1>Anthony Blanton</h1>
+        <a href="#"><h1>Anthony Blanton</h1></a>
         <nav>
             <!--  router-link component for navigation. -->
-            <router-link to="/#">Home</router-link>
-            <router-link to="/Projects">Projects</router-link>
-            <router-link to="/about">About</router-link>
-            <router-link to="/contact">Contact</router-link>
+            <router-link v-scroll-to="'body'" to="/#">Home</router-link>
+            <router-link v-scroll-to="'body'" to="/projects">Projects</router-link>
+            <router-link v-scroll-to="'body'" to="/about">About</router-link>
+            <router-link v-scroll-to="'body'" to="/contact">Contact</router-link>
         </nav>
     </header>
     <!-- route outlet -->
@@ -17,14 +17,23 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import VueScrollTo from 'vue-scrollto'
 import VueSticky from 'vue-sticky'
+
+//scrolls to top of body when route changes
+Vue.use(VueScrollTo, {
+    duration: 1
+})
+
 export default {
      directives: {
-        'sticky': VueSticky,
+        'sticky': VueSticky, //sticks header to top of view
     },
         data () {
             return {
             }
-        }
+        },
+        
     }    
 </script>
